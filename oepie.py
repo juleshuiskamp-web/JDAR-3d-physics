9cddbddb4593074492a2801938b478193ef5f5ea
GitBodie <12rubendejongh@gmail.com>
2026-04-15 16:10:52 +0200

    Delete oepie.py

--- oepie.py
+++
@@ -1,8 +0,0 @@
-import math as m
-while True:
-    zoom = float(input("zoom"))
-    screenWidth = int(input("screenwidth"))
-    power = round(m.log((screenWidth*zoom),10))
-    relScreenWidth = (screenWidth*zoom) /(10**power) 
-    print(f"power{power},scientific{relScreenWidth}")
-    print(f"screenwidth{screenWidth*zoom}")
