8afc3dd8b587f451f608215cffa5f7b438a39d26
juleshuiskamp-web <jules.huiskamp@hotmail.com>
2026-05-19 16:05:49 +0200

    Update import json.py

--- import json.py
+++ import json.py
@@ -1,5 +1,5 @@
 import json
-
+#Import de Json bestand 
 with open("object_data.json", "r") as file:
     data = json.load(file)
 
@@ -10,7 +10,7 @@ name = obj["naam"]
 x = obj["position"]["x"]
 y = obj["position"]["y"]
 z = obj["position"]["z"]
-
+#Checkt of de coordinaten een int of float zijn, als dat zo is print het de packet, anders geeft het een error
 if (
     isinstance(x,(int,float)) and
     isinstance(y,(int,float)) and
