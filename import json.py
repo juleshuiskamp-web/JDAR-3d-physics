a3ce408edee2f90225bed9361a495e74569230fd
juleshuiskamp-web <jules.huiskamp@hotmail.com>
2026-05-19 16:02:41 +0200

    Create import json.py

---
+++ import json.py
@@ -0,0 +1,28 @@
+import json
+
+with open("object_data.json", "r") as file:
+    data = json.load(file)
+
+
+obj = data["object"]
+
+name = obj["naam"]
+x = obj["position"]["x"]
+y = obj["position"]["y"]
+z = obj["position"]["z"]
+
+if (
+    isinstance(x,(int,float)) and
+    isinstance(y,(int,float)) and
+    isinstance(z,(int,float))
+):
+    print ("Coordianten zijn goed")
+    packet = {
+    "naam": obj["naam"],
+    "x": x,
+    "y": y,
+    "z": z
+    }
+    print(packet)
+
+else: print ("Error coordianten zijn niet goed")
